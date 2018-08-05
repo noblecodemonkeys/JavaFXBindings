e13bc757e014119be45e832eea8cc28aad8d25a4
Ashley <Ashley ashley.noble@gmail.com>
2018-08-04 20:22:12 -0400

    The initial JavaFX Databinding Tutorial on unidirectional databinding

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,55 @@
 package sample;
 
-public class Controller {
+import javafx.beans.property.IntegerProperty;
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Controller implements Initializable
+{
+
+    @FXML
+    Label lblLabel;
+    private final StringProperty textValue = new SimpleStringProperty("waiting for input");
+
+    @FXML
+    private void handleButtonAction(ActionEvent event)
+    {
+        System.out.println("You clicked me!");
+
+        setTextValue("button clicked");
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources)
+    {
+        //TODO Uncomment this  to activate the binding in code
+        //remember to also uncoment out the fxml label
+        //  lblLabel.textProperty().bind(textValueProperty());
+
+    }
+
+    public String getTextValue()
+    {
+        return textValue.get();
+    }
+
+    public StringProperty textValueProperty()
+    {
+        return textValue;
+    }
+
+    public void setTextValue(String textValue)
+    {
+        this.textValue.set(textValue);
+    }
+
+
 }
